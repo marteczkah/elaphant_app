f5540a223a33055d65f1a111fd49a382bee7fa11
Marta <48037981+marteczkah@users.noreply.github.com>
2021-08-05 16:36:41 +0200

    Added setup files

---
+++ elephants.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+from tkinter import X
+from tkinter.constants import RIGHT
+
+# window setup
+
+# functions
+
+# title
+
+# image
+
+# 'subtitle'
+
+# user input
+
+# button
+
+# result
+
+# making sure the window opens
+
+
